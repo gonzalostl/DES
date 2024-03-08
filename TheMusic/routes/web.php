c9b286689e7661c9ad7a4a0894a5b7fca1d54477
gsanlop705 <gsanlop705@g.educaand.es>
2024-03-09 00:02:24 +0100

    s

--- TheMusic/routes/web.php
+++ TheMusic/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\CancionesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +14,17 @@ use App\Http\Controllers\LoginController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('/', function () {
+    return view('welcome');
+});
 
+Route::get('/canciones', [CancionesController::class, 'index'])->name('canciones.index');
+Route::get('/canciones/create', [CancionesController::class, 'create'])->name('canciones.create');
+Route::post('/canciones', [CancionesController::class, 'store'])->name('canciones.store');
+Route::get('/canciones/{id}', [CancionesController::class, 'show'])->name('canciones.show');
+Route::get('/canciones/{id}/edit', [CancionesController::class, 'edit'])->name('canciones.edit');
+Route::put('/canciones/{id}', [CancionesController::class, 'update'])->name('canciones.update');
+Route::delete('/canciones/{id}', [CancionesController::class, 'destroy'])->name('canciones.destroy');
 
 Route::view('/login', "login")->name('login');
 Route::view('/registro', "register")->name('registro');
